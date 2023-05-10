544eef925cbaa68473d77545c426186059ebc64d
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-05-09 17:46:25 -0700

    0-positive_or_negative.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 for i in range(100):
-    print("{:02d}".format(i), end="")
-    if i != 99:
-        print(", ", end="")
+    if i == 99:
+        print("{:02d}".format(i))
     else:
-        print("\n", end="")
+        print("{:02d}, ".format(i), end=""
