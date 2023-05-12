d61dc67f1bdf7f8d32b14e07929f02b5f3a250d9
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-05-11 18:43:19 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from add_0 import add
 
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+
+if __name__ == "__main__":
     a = 1
     b = 2
-
     result = add(a, b)
-
     print(f"{a} + {b} = {result}")
+
