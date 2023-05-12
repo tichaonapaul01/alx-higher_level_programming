7f0d2c346c06610b641a50789d8754ead279eafd
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-05-11 18:25:55 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-from add_0 import add
+if __name__ == "__main__":
+    from add_0 import add
 
-a = 1
-b = 2
+    a = 1
+    b = 2
 
-result = add(a, b)
+    result = add(a, b)
 
-print(f"{a} + {b} = {result}")
+    print(f"{a} + {b} = {result}")
