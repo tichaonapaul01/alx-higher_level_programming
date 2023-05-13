5e622fd4cec170422dc89b1b2e74fcde491ca505
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-05-13 11:42:02 -0700

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 for i in range(100):
-    if i == 99:
-        print("{:02d}".format(i))
+    print("{:02d}".format(i), end="")
+    if i != 99:
+        print(", ", end="")
     else:
-        print("{:02d}, ".format(i), end=""
+        print("\n", end="")
