adabbd2dd9ae122f694751fce5617ed4fd6dc118
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-05-13 02:05:13 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 import sys
 
-args = sys.argv[1:]
-num_args = len(args)
-
-print(f"Number of argument{'s' if num_args != 1 else ''}: {num_args}", end='')
-if num_args == 0:
-    print('.')
-else:
-    print(':')
-
-    for i, arg in enumerate(args, start=1):
-        print(f"{i}: {arg}")
+if __name__ == "__main__":
+    num_args = len(sys.argv) - 1
+    if num_args == 0:
+        print("Number of argument(s): 0.")
+        print(".")
+    elif num_args == 1:
+        print("Number of argument(s): 1. Argument:")
+        print("1: " + sys.argv[1])
+    else:
+        print("Number of argument(s): " + str(num_args) + ". Arguments:")
+        for i in range(1, num_args + 1):
+            print(str(i) + ": " + sys.argv[i])
