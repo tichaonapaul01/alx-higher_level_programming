a6635df2f14f26d03cd2324b690edb19847e8843
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-05-13 14:13:20 -0700

    101-remove_char_at.py

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,6 @@
+def remove_char_at(s, n):
+    # Check if the index is valid
+    if n < 0 or n >= len(s):
+        return s
+    # Create a new string with the characters before and after the index
+    return s[:n] + s[n+1:]
