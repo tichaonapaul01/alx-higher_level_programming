e40bb33fe1025f4e4ddec3d6baea57e5ca6ae417
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-05-13 14:06:37 -0700

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for i in range(90, 64, -1):
-    print(chr(i + 32 if i % 2 == 0 else i), end='')
+for i in range(ord('z'), ord('a') - 1, -1):
+    if i % 2 != 0:
+        i -= 32
+    print("{:c}".format(i), end="")
