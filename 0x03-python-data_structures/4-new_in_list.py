fa5ea3b25d38958cb4418abaf95ed03dbd90f6e3
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-05-15 12:38:33 -0700

    4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,6 @@
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list.copy()
+    new_list = [x for x in my_list]
+    new_list[idx] = element
+    return new_list
