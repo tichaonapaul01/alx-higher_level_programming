529237f508d647da602bcb898f9643f4c66366c4
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-05-15 13:35:19 -0700

    9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+
+    max_val = my_list[0]
+
+    for i in range(1, len(my_list)):
+        if my_list[i] > max_val:
+            max_val = my_list[i]
+
+    return max_val
