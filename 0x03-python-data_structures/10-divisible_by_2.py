ac273fd6d8f47bcb31e28993ab851ad66c772dd0
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-05-15 13:44:15 -0700

    10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+def divisible_by_2(my_list=[]):
+    # Create a new list to store the result
+    result = []
+    # Iterate through the list and check if each integer is a multiple of 2
+    for num in my_list:
+        if num % 2 == 0:
+            result.append(True)
+        else:
+            result.append(False)
+            # Return the result
+    return result
