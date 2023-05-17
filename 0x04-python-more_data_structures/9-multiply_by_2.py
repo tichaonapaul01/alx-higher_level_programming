b55279099983372dd89e8c207491c4111a1a0278
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-05-17 15:41:34 -0700

    9-multiply_by_2.py

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    multiplied_dictionary = {}
+    for key, value in a_dictionary.items():
+        multiplied_dictionary[key] = value * 2
+    return multiplied_dictionary
