e60cd8a6d3ca21a5064631f9bc500e7bff34714c
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-05-30 14:41:30 -0700

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-class Square:
-    """an empty class Square that
-       defines a square
-    """
-    pass
 
+class Square:
+    """An empty class Square that defines a square"""
+    pass
 
 s = Square()
 print(s)
