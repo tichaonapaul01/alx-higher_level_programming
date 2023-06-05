7a8e4a941cc4488be94986b98e104df8302d7ef0
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-06-05 13:16:39 -0700

    0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+Write an empty class Rectangle that defines a rectangle:
+'''
+
+
+class Rectangle:
+    def __init__(self):
+        pass
+
+    def area(self):
+        pass
+
+    def perimeter(self):
+        pass
