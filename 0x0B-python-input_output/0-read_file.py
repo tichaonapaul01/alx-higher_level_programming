ab8dfc9a3cfc0216af9f285ce537a61d5e28666e
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-06-13 15:25:16 -0700

    initial commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-
-'''
-Write a function that reads a text file (UTF8) and prints it to stdout:
-'''
+"""
+0-read_file.py
+a function that reads a text file (UTF8) and prints it to stdout
+"""
 
 
 def read_file(filename=""):
-    with open(filename, "r", encoding="utf-8") as file:
-        content = file.read()
-        print(content)
+    """read file utf8 con with """
+    with open(filename, encoding='UTF-8') as f:
+        for line in f:
+            print(line, end='')
